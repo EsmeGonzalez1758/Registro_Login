662a3721ff9cd4f244a29734a8bb922434b99f2a
EsmeGonzalez1758 <139596088+EsmeGonzalez1758@users.noreply.github.com>
2023-07-19 21:55:46 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+		
+	<div class="row">
+	<div class="col-md-12">
+			<h2>REGISTRO Y LOGIN</h2>
+			<p class="lead">Sistema de Registro y Login </p>
+			<p>Instrucciones :</p>
+			<ol>
+				<li>Registrate en la opcion "registro".</li>
+				<li>Inicie sesion en la opcion "login".</li>
+				<li>Para finalizar la sesion, click en la opcion "salir" .</li>
+			</ol>
+			<br>
+			
+
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
